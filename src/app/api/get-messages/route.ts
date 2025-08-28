c84752cd51dea89700f218c926c7b3a8a1c79aa6
Mehul <mehulprajapati2802@gmail.com>
2025-08-28 18:28:55 +0530

    added get and send message route handlers

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,58 @@
+import UserModel from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+import { authOptions } from '../auth/[...nextauth]/options'
+import { getServerSession } from "next-auth";
+import {User} from 'next-auth'
+import mongoose from "mongoose";
+
+
+export async function GET(request: Request) {
+
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+
+        return Response.json(
+            { success: false, message: 'Not authenticated' },
+            { status: 401 }
+        );
+    }
+
+    const _user: User = session?.user;
+    const userId = new mongoose.Types.ObjectId(_user._id);
+
+    try {
+
+        const user = await UserModel.aggregate([
+            {$match: {_id: userId}},
+            {$unwind: '$messages'},
+            {$sort: {'messages.createdAt': -1}},
+            {$group: { _id: '$_id', messages: { $push: '$messages' } } },
+        ]).exec();
+
+        if (!user || user.length === 0) {
+            return Response.json(
+                { message: 'User not found', success: false },
+                { status: 404 }
+            );
+        }
+
+        return Response.json(
+            { messages: user[0].messages },
+            {
+                status: 200,
+            }
+        );
+    }
+    catch (error) {
+
+        console.error('Error retrieving messages:', error);
+
+        return Response.json(
+            { success: false, message: 'Error retrieving messages' },
+            { status: 500 }
+        );
+    }
+
+}
